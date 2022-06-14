b1d558d1c5caf8116c61e3c94be3524f4bf824bc
JayMartMedia <43322309+JayMartMedia@users.noreply.github.com>
2022-06-14 08:46:47 -0400

    Update removeMatchesFromImmutableArray.js

--- javascript/removeMatchesFromImmutableArray.js
+++ javascript/removeMatchesFromImmutableArray.js
@@ -1,1 +1,11 @@
-
+function removeMatchesFromImmutableArray(arr, predicate){
+    let index = 0;
+    while(index < arr.length){
+        const item = arr[index]
+        if(predicate(item)){
+            arr.splice(index, 1);
+        }else{
+            index += 1;
+        }
+    }
+}
